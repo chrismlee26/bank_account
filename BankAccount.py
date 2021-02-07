35886e90ebe8a22e0a5472bd57ef8f22f878df0d
CHRIS_LEE <chrismlee87@gmail.com>
2021-02-07 13:37:23 -0800

    partially working

--- BankAccount.py
+++ BankAccount.py
@@ -3,21 +3,19 @@ import random
 
 class BankAccount:
     def __init__(self, full_name, account_number, routing_number, balance):
-        self.full_name = full_name
-        self.account_number = account_number
+        self.full_name = "full_name"
+        self.account_number = random.randint(100000000, 999999999)
         self.routing_number = routing_number
-        balance = 0
+        self.balance = 0
 
-        account_number = random.randint(100000000, 999999999)
-
-    def deposit(self, amount):
-        deposit += amount
+    def deposit(self, amount, balance):
+        self.balance += amount
         print(f"Amount Deposited: ${amount}")
 
-    def withdraw(self, balance, amount):
-        withdraw -= amount
-        if balance < amount:
-            balance -= 10
+    def withdraw(self, amount, balance):
+        self.balance -= amount
+        if self.balance < amount:
+            self.balance -= 10
             print("Insufficient Funds.")
         print(f"Amount Withdrawn: ${amount}")
 
@@ -25,10 +23,11 @@ class BankAccount:
         print(f"Balance: {balance} ")
 
     def add_interest(self, interest, balance):
-        interest = balance * 0.083
+        self.interest = self.balance * 0.083
 
     def print_receipt(self, full_name, account_number, routing_number, balance):
         print(f"{full_name}\nAccount No.: {account_number}\nRouting No.:{routing_number}\nBalance: ${balance}")
 
 
-BankAccount(chris, 12345, 12345, 100)
+chris = BankAccount("asdfg asdfg", 12345, 12345, 12)
+print(chris)

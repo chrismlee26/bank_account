e9d41e75246a1c29f0a29c44bbaa220f3208870b
CHRIS_LEE <chrismlee87@gmail.com>
2021-02-08 14:21:33 -0800

    master

--- BankAccount.py
+++ BankAccount.py
@@ -2,32 +2,45 @@ import random
 
 
 class BankAccount:
-    def __init__(self, full_name, account_number, routing_number, balance):
-        self.full_name = "full_name"
+    def __init__(self, full_name, balance):
+        self.full_name = full_name
         self.account_number = random.randint(100000000, 999999999)
-        self.routing_number = routing_number
-        self.balance = 0
+        self.routing_number = 631492825
+        self.balance = balance
 
-    def deposit(self, amount, balance):
+    def deposit(self, amount):
+        self.amount = 0
         self.balance += amount
         print(f"Amount Deposited: ${amount}")
 
-    def withdraw(self, amount, balance):
+    def withdraw(self, amount):
         self.balance -= amount
         if self.balance < amount:
             self.balance -= 10
             print("Insufficient Funds.")
         print(f"Amount Withdrawn: ${amount}")
 
-    def get_balance(self, balance):
-        print(f"Balance: {balance} ")
+    def get_balance(self):
+        print(f"Balance: {self.balance}")
 
-    def add_interest(self, interest, balance):
-        self.interest = self.balance * 0.083
+    def add_interest(self):
+        self.interest = self.balance * 0.00083
+        print(f"Interest: ${self.interest}")
 
-    def print_receipt(self, full_name, account_number, routing_number, balance):
-        print(f"{full_name}\nAccount No.: {account_number}\nRouting No.:{routing_number}\nBalance: ${balance}")
+    def print_receipt(self):
+        print(f"{self.full_name}\nAccount No.: {self.account_number}\nRouting No.:{self.routing_number}\nBalance: ${self.balance}")
 
 
-chris = BankAccount("asdfg asdfg", 12345, 12345, 12)
-print(chris)
+print("-----Example #1-----")
+chris = BankAccount("chris", 12)
+chris.deposit(5000)
+chris.print_receipt()
+print("-----Example #2-----")
+bob = BankAccount("bob", 400)
+bob.deposit(100)
+bob.withdraw(600)
+bob.print_receipt()
+print("-----Example #3-----")
+bone = BankAccount("bone", 50)
+bone.add_interest()
+bone.print_receipt()

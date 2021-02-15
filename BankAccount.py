f9f9cab306e901235436f67771a0080ffbaefc35
CHRIS_LEE <chrismlee87@gmail.com>
2021-02-14 19:50:16 -0800

    added comments, notes and updated readme

--- BankAccount.py
+++ BankAccount.py
@@ -1,46 +1,68 @@
 import random
 
+# This is the class that initializes the BankAccount object
+
 
 class BankAccount:
-    def __init__(self, full_name, balance):
+    # @params: "Full Name" & Balance (default is 0)
+    def __init__(self, full_name, balance=0):
         self.full_name = full_name
+        # This needs to censor the first few digits with ***
         self.account_number = random.randint(10000000, 99999999)
         self.routing_number = 631492825
         self.balance = balance
 
-    def deposit(self, amount):
-        self.amount = 0
-        self.balance += amount
+    # deposit function
+    # @params: amount, given by user, 0 is default
+    def deposit(self, amount=0):
+        self.amount = amount
+        self.balance += amount  # Amount is added to balance
         print(f"Amount Deposited: ${amount}")
 
-    def withdraw(self, amount):
+    # withdraw function
+    # @ params: amount, given by user, 0 is default
+    def withdraw(self, amount=0):
         self.balance -= amount
+        # Maybe we can add in functionality that will allow overdraft up do a specific number then just reject.
         if self.balance < amount:
             self.balance -= 10
-            print("Insufficient Funds.")
+            print("Insufficient Funds. Overdraft Fee of $10 has been charged.")
         print(f"Amount Withdrawn: ${amount}")
 
     def get_balance(self):
+        # Missing a return value?
         print(f"Hi beautiful, your balance is: {self.balance}")
 
+    # This needs to add to balance immediately when the class is created. Strange but ok.
     def add_interest(self):
         self.interest = self.balance * 0.00083
         print(f"Interest: ${self.interest}")
 
     def print_receipt(self):
-        print(f"{self.full_name}\nAccount No.: {self.account_number}\nRouting No.:{self.routing_number}\nBalance: ${self.balance}")
+        print(f"Name: {self.full_name}\nAccount No.: {self.account_number}\nRouting No.:{self.routing_number}\nBalance: ${self.balance}")
 
 
 print("-----Example #1-----")
-chris = BankAccount("Chris", 12)
+chris = BankAccount("Chris Lee")
 chris.deposit(5000)
 chris.print_receipt()
 print("-----Example #2-----")
-pringles = BankAccount("Pringles", 400)
-pringles.deposit(100)
+pringles = BankAccount("Pringles Dog")
+pringles.deposit(550)
 pringles.withdraw(600)
 pringles.print_receipt()
 print("-----Example #3-----")
-bone = BankAccount("Bone", 50)
+bone = BankAccount("Bone Bone")
+bone.get_balance()
 bone.add_interest()
 bone.print_receipt()
+
+
+# Add more things to Readme
+# Add comments in code
+# Initial balance should start at 0
+# self.amount = 0 does nothing, can remove
+# check withdraw before deposit -- maybe add an if function to now allow
+# get_balance has no return value
+# balance needs to update to add interest
+# print_receipt needs to hide first ##'s in the account_number
--- README.md
+++ README.md
@@ -1,23 +1,28 @@
 ## Welcome
 
+![GitHub top language](https://img.shields.io/github/languages/top/chrismlee26/bank_account?style=for-the-badge)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/chrismlee26/bank_account?style=for-the-badge)
+![GitHub last commit](https://img.shields.io/github/last-commit/chrismlee26/bank_account?style=for-the-badge)
+
 This project is my bank account program in Python3. The Project encompasses OOP Methods & Attributes as Assignment #1 for CS1.1 (OOP).
 
 ## Run
 
 $ "python3 BankAccount.py" to run.
-$ Or just press the run button to run.
+$ Or just press the run button in VScode
 
-## Operations
+## Operation
 
-1. Define a **BankAccount** class
-2. The class should have the following attributes:
+1. The **BankAccount** class creates an instance of BankAccount when called.
+2. The **BankAccount** class takes the @params "Full Name" (string), Balance (int). Balance is defaulted to 0.
+3. The class has the following attributes:
 
 - **full_name** : Full name of Owner
 - **account_number** : Randomly generated 8 digit umber. Unique Per Account
 - **routing_number** : 9 Digit Number, same for all accounts.
 - **balance** : The balance of the account, should start at 0
 
-3. Methods for the BankAccount class:
+4. Methods for the BankAccount class:
 
 - **deposit** takes one parameter 'amount' and will add 'amount' to the 'balance'. Prints the message: "Amount Deposited: $X.XX"
 - **withdraw** takes one parameter 'amount' and will subtract 'amount' from the 'balance'. Prints the message: "Amount Withdrawn: $X.XX". If the user tries to withdraw an amount greater than current balance, prints "Insufficicent Funds" and charges and overdraft fee of $10.
@@ -25,4 +30,4 @@ $ Or just press the run button to run.
 - **add_interest** adds interest to the users balance. The annual interest rate is 1%.
 - **print_recept** prints a receipt with the account name, number and balance in a specific order.
 
-4. There are 3 examples of BankAccount() objects to demonstrate methods.
+5. There are 3 examples of BankAccount() objects to demonstrate methods. These are found on lines 45-58.
